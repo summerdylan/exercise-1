80c21e740eba77336a2850605ac76991b481bbac
Dailulucy <dailulucy@163.com>
2015-09-29 23:46:13 +0800

    0929
    add muedu

--- web/index.dart
+++ web/index.dart
@@ -1,9 +1,9 @@
 import 'dart:html';
-import 'dart:math' as math;
+
 void main() {
   querySelector('#sample_text_id')
     ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..onClick.listen(reverseText);
 }
 
 void reverseText(MouseEvent event) {
@@ -13,17 +13,4 @@ void reverseText(MouseEvent event) {
     buffer.write(text[i]);
   }
   querySelector('#sample_text_id').text = buffer.toString();
-}
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
 }
