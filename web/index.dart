c6056f80ba99343f95e511d57805aa83889649b0
刘傲然 <823219343@qq.com>
2015-09-29 22:51:12 +0800

    try
    之前的练习

--- web/index.dart
+++ web/index.dart
@@ -2,8 +2,8 @@ import 'dart:html';
 import 'dart:math' as math;
 void main() {
   querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..text = '3+6='
+    ..onClick.listen(add);
 }
 
 void reverseText(MouseEvent event) {
@@ -14,16 +14,24 @@ void reverseText(MouseEvent event) {
   }
   querySelector('#sample_text_id').text = buffer.toString();
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
+
+
+
+void randomSelectStu(MouseEvent event) {
+  var stuMap = {
+    0:1213344345,
+    1:6786876872
   };
   var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+
+  var randomID = random.nextInt(2);
+  var stuID = stuMap[randomID];
+  querySelector('#sample_studentid_id').text = stuID.toString();
 }
+
+  void add(MouseEvent event) {
+    var number1 = 3;
+    var number2 = 6;
+    var number3 = number1 + number2;
+    querySelector('#sample_sum_id').text = number3.toString();
+  }
