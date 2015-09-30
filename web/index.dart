a7c008670f1634c2fdca11ef4ac8c2944b26a0f4
bestwishes369 <673800843@qq.com>
2015-09-30 21:43:11 +0800

    9-30 5050
    完成1加到100运算，下一步运用文本框输入输出数字

--- web/index.dart
+++ web/index.dart
@@ -1,29 +1,30 @@
 import 'dart:html';
 import 'dart:math' as math;
+
 void main() {
   querySelector('#sample_text_id')
     ..text = 'Click me!'
-    ..onClick.listen((MouseEvent e)=>randomStudentID("you!",e));
+    ..onClick.listen(reverseText)
+    ..onClick.listen(getnumber);
 }
 
-void reverseText(MouseEvent event) {
-  var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-  for (int i = text.length - 1; i >= 0; i--) {
-    buffer.write(text[i]);
-  }
-  querySelector('#sample_text_id').text = buffer.toString();
+int yunSuan(){
+  int i,s=0;
+  for(i=1;i<=100;i++)s=s+i;
+  return s;
 }
-void randomStudentID(String showStr,MouseEvent even){
-  var students={
-    0:10001,
-    1:10002,
-    2:100003,
-    3:100004,
-    4:100005,
-    5:100005
-  };
-  var random = new math.Random();
-  var getYou=students[random.nextInt(6)];
-  querySelector('#sample_studentid_id').text = getYou.toString()+showStr;
+void getnumber(mouseevent event){//得到文本框中数字
+  string a=querySelector('#text1').text;
+  shuchu(a);
 }
+void shuchu(string s){
+  querySelector("#sample_text_id")
+  ..text=s;
+}
+void reverseText(mouseevent event) {
+  int a=yunSuan();
+  String ss="1 add to 100 is:"+a.toString();
+  querySelector('#sample_add_id').text=ss;
+}
+
+
