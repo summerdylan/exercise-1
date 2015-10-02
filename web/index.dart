5c1b2f0f5038bd8873810037f01c51827635ca18
zhangyilei <378008891@qq.com>
2015-10-02 22:27:32 +0800

    change

--- web/index.dart
+++ web/index.dart
@@ -2,9 +2,12 @@ import 'dart:html';
 import 'dart:math' as math;
 void main(){
   var c;
+  int s=0;
   querySelector('#sample_text_id')
     ..text="Add number"
     ..onClick.listen((MouseEvent e)=>randomSelectStu(c,e));
+  querySelector('#sample_text1_id')
+    ..onClick.listen((MouseEvent e)=>AddNumber(s,e));
 }
 void randomSelectStu(String c, MouseEvent event){
   var A,B;
@@ -14,8 +17,10 @@ void randomSelectStu(String c, MouseEvent event){
   var b=int.parse(B);
   c=a+b;
   document.getElementById("sample_studentid_id").value=c.toString();
-  printNumber(c);
 }
-void printNumber(num number) {
-  print('The number is $number.');
+void AddNumber(int s, MouseEvent event){
+  int i;
+  for(i=1;i<=100;i++)
+    s=s+i;
+  document.getElementById("sample_add_id").value=s.toString();
 }
