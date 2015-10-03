d220334c8c9d996678c83176595769f25a7c30ac
lisulin <1126361165@qq.com>
2015-10-03 12:10:48 +0800

    10130340222
    Add 1 To 100

--- web/index.dart
+++ web/index.dart
@@ -1,42 +1,28 @@
 import 'dart:html';
 import 'dart:math' as math;
 void main() {
-  querySelector('#sample_text_id')
-    ..text = 'Click me!'
-    ..onClick.listen(randomSelectStu);
-  querySelector('#sample_add_id').text =addNumber(1,2).toString();
-
-  querySelector('#sample_button_id')
-    ..text = 'Add'
+  querySelector('#button_add_id1')
+    ..text = '='
     ..onClick.listen(addTwoNumber);
+
+  querySelector('#button_add_id2')
+    ..text = '='
+    ..onClick.listen(addOneToHundred);
+}
+
+void addTwoNumber(MouseEvent event){
+  vax x=int.parse(querySelector('#a').value);
+  vax y=int.parse(querySelector('#b').value);
+  querySelector('#c').text=addNumber(x,y).toString();
 }
 
 int addNumber(x,y){
   return(x+y);
 }
-void addTwoNumber(MouseEvent event){
-  vax x=int.parse(querySelector('#number_a').value);
-  vax y=int.parse(querySelector('#number_b').value);
-  querySelector('#sample_text_id2').text=addNumber(x,y).toString();
-}
 
-void reverseText(MouseEvent event) {
- var text = querySelector('#sample_text_id').text;
-  var buffer = new StringBuffer();
-    for (int i = text.length - 1; i >= 0; i--) {
- buffer.write(text[i]);
-  }
- querySelector('#sample_text_id').text = buffer.toString();
-}
-
-void randomSelectStu(MouseEvent event) {
-  var stuMap={
-    0:1213344345,
-    1:6786876872
-  };
-  var random = new math.Random();
-
-  var randomID=random.nextInt(2);
-  var stuID=stuMap[randomID];
-  querySelector('#sample_studentid_id').text =stuID.toString();
+void addOneToHundred(MouseEvent event){
+   int sum=0;
+  for(int i=1;i<=100;i++)
+   sum=sum+i;
+   querySelector('#result').text=sum.toString();
 }
